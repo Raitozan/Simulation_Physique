d3cd5c6a7bf57e1f4770edb84ece7b5023586189
Nathan Desages <nathan.desages@gmail.com>
2018-10-30 16:03:41 +0100

    ajout des nodes et des bones avec possibilité déplacement bones via souris

---
+++ Assets/Scipts/Bone.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bone : MonoBehaviour {
+
+	Node NodeA;
+	public Node NodeB;
+
+	LineRenderer lr;
+
+	Vector3 lastAPos;
+	Vector3 lastBPos;
+
+	// Use this for initialization
+	void Start ()
+	{
+		NodeA = gameObject.GetComponentInParent<Node>();
+		lr = gameObject.GetComponent<LineRenderer>();
+
+		lr.SetPosition(0, NodeA.transform.position);
+		lastAPos = NodeA.transform.position;
+		lr.SetPosition(1, NodeB.transform.position);
+		lastBPos = NodeB.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(lastAPos != NodeA.transform.position || lastBPos != NodeB.transform.position)
+		{
+			lr.SetPosition(0, NodeA.transform.position);
+			lastAPos = NodeA.transform.position;
+			lr.SetPosition(1, NodeB.transform.position);
+			lastBPos = NodeB.transform.position;
+		}
+	}
+}
