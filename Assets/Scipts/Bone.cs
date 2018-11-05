a4037b675ac6fe597bd63b4c8095964066f0624c
Nathan Desages <nathan.desages@gmail.com>
2018-11-05 12:32:19 +0100

    reprise du projet: ajout d'une chaine de noeud qui suit une target

--- Assets/Scipts/Bone.cs
+++ Assets/Scipts/Bone.cs
@@ -4,34 +4,15 @@ using UnityEngine;
 
 public class Bone : MonoBehaviour {
 
-	Node NodeA;
-	public Node NodeB;
-
-	LineRenderer lr;
-
-	Vector3 lastAPos;
-	Vector3 lastBPos;
+	public Transform attachedNode;
 
 	// Use this for initialization
-	void Start ()
-	{
-		NodeA = gameObject.GetComponentInParent<Node>();
-		lr = gameObject.GetComponent<LineRenderer>();
-
-		lr.SetPosition(0, NodeA.transform.position);
-		lastAPos = NodeA.transform.position;
-		lr.SetPosition(1, NodeB.transform.position);
-		lastBPos = NodeB.transform.position;
+	void Start () {
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(lastAPos != NodeA.transform.position || lastBPos != NodeB.transform.position)
-		{
-			lr.SetPosition(0, NodeA.transform.position);
-			lastAPos = NodeA.transform.position;
-			lr.SetPosition(1, NodeB.transform.position);
-			lastBPos = NodeB.transform.position;
-		}
+		
 	}
 }
---
+++ Assets/Scipts/NodeChain.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeChain : MonoBehaviour
+{
+
+	List<Transform> nodes;
+	List<float> lengths;
+
+	Vector3 target;
+
+	LineRenderer lr;
+
+	// Use this for initialization
+	void Start()
+	{
+		Init();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		UpdateTarget();
+		if (transform.position != target)
+			MoveChain();
+	}
+
+	public void Init()
+	{
+		nodes = new List<Transform>();
+		lengths = new List<float>();
+		target = transform.position;
+		Transform actualNode = transform;
+
+		while (!actualNode.CompareTag("AnchorNode"))
+		{
+			Transform lastNode = actualNode;
+			actualNode = actualNode.parent;
+
+			lengths.Add(Vector3.Distance(lastNode.position, actualNode.position));
+
+			nodes.Add(actualNode);
+		}
+
+		lr = GetComponent<LineRenderer>();
+		lr.positionCount = nodes.Count + 1;
+		UpdateLineRenderer();
+	}
+
+	public void UpdateTarget()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			target.z = 0.0f;
+		}
+	}
+
+	public void MoveChain()
+	{
+		Vector3 anchorPos = nodes[nodes.Count - 1].position;
+
+		transform.position = target;
+		Vector3 lastNode = target;
+		for (int i = 0; i < nodes.Count; i++)
+		{
+			nodes[i].position = lastNode + (nodes[i].position - lastNode).normalized * lengths[i];
+			lastNode = nodes[i].position;
+		}
+
+		nodes.Reverse();
+		lengths.Reverse();
+
+		nodes[0].position = anchorPos;
+		lastNode = anchorPos;
+		for (int i = 1; i < nodes.Count; i++)
+		{
+			nodes[i].position = lastNode + (nodes[i].position - lastNode).normalized * lengths[i - 1];
+			lastNode = nodes[i].position;
+		}
+		transform.position = lastNode + (transform.position - lastNode).normalized * lengths[lengths.Count - 1];
+
+		nodes.Reverse();
+		lengths.Reverse();
+
+		UpdateLineRenderer();
+	}
+
+	public void UpdateLineRenderer()
+	{
+		LineRenderer lr = GetComponent<LineRenderer>();
+		lr.SetPosition(0, transform.position);
+		int i = 1;
+		foreach(Transform node in nodes)
+			lr.SetPosition(i++, node.position);
+	}
+}
